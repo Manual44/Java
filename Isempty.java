0db48ccfada85f9a4216fd4a2cc55119968b3a86
Manual44 <mrisman.maulana123@gmail.com>
2024-11-16 12:55:52 +0700

    Add files via upload

---
+++ Isempty.java
@@ -0,0 +1,18 @@
+// Class Utama
+public class isempty {
+  public static void main(String[] args) {
+    
+    // Membuat Objek String
+    String text1 = "";
+    String text2 = "Hello, World";
+      
+    // Menampilkan Output
+    System.out.println("Output pertama: " + text1.isEmpty());
+    System.out.println("Output kedua: " + text2.isEmpty());
+      
+  }
+}
+
+// OUTPUT //
+// Output pertama: true
+// Output kedua: false
